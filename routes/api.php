c2f8add530673583208b1c364a12a6c1166c576a
enriquemdev <enriquemunozdev@gmail.com>
2024-06-05 04:31:56 -0600

    fc

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+    // La ruta test es para probar si sirve la autenticacion jwt
+    // En el controlador, hay que meterla en el except del metodo constructor si se desea que no requiera que se le pase el token jwt para que funcione
+    Route::get('test', 'test');
+});
+
